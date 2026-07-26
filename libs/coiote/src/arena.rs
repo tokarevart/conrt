d71d624f8c4303ce237fea68d86b66b96a3c30db
Tokarev Artem <tokarev28.art@gmail.com>
2026-07-26 21:45:28 +0500

    arena: rename base_ptr->buf, max_capacity->capacity, remove ARENA_MAX and PAGE_SIZE, add ARENA_ALIGN

--- libs/coiote/src/arena.rs
+++ libs/coiote/src/arena.rs
@@ -7,9 +7,8 @@ use std::alloc::dealloc;
 use std::cell::Cell;
 use std::num::NonZeroU32;
 
-pub const ARENA_MAX: u32 = u32::MAX;
+pub const ARENA_ALIGN: usize = 1 << 31;
 pub const FOOTER_SIZE: u32 = size_of::<AllocFooter>() as u32;
-pub const PAGE_SIZE: usize = 4096;
 
 #[derive(Clone, Copy)]
 #[repr(transparent)]
@@ -55,22 +54,22 @@ const _: () = assert!(size_of::<Option<ArenaAlloc>>() == size_of::<ArenaAlloc>()
 pub struct ArenaAllocSlice(pub NonNull<[u8]>);
 
 pub struct Arena {
-    base_ptr: NonNull<u8>, // page aligned
+    buf: NonNull<u8>, // page aligned
     offset: Cell<u32>,
-    max_capacity: u32,
+    capacity: u32,
 }
 
 impl Arena {
     pub fn new(capacity: u32) -> Self {
-        let layout = Layout::from_size_align(capacity as usize, PAGE_SIZE).unwrap();
+        let layout = Layout::from_size_align(capacity as usize, ARENA_ALIGN).unwrap();
         let ptr = unsafe { alloc(layout) };
         if ptr.is_null() {
             std::alloc::handle_alloc_error(layout);
         }
         Self {
-            base_ptr: unsafe { NonNull::new_unchecked(ptr) },
+            buf: unsafe { NonNull::new_unchecked(ptr) },
             offset: Cell::new(0),
-            max_capacity: capacity,
+            capacity,
         }
     }
 
@@ -81,15 +80,10 @@ impl Arena {
         let aligned = (self.offset.get() + align - 1) & !(align - 1);
         let padded = aligned + size.get();
         let total = padded.checked_add(FOOTER_SIZE)?;
-        if total > self.max_capacity {
+        if total > self.capacity {
             return None;
         }
-        let footer_addr = unsafe {
-            self.base_ptr
-                .as_ptr()
-                .add(padded as usize)
-                .cast::<AllocFooter>()
-        };
+        let footer_addr = unsafe { self.buf.as_ptr().add(padded as usize).cast::<AllocFooter>() };
         unsafe {
             footer_addr.write_unaligned(AllocFooter::new(padded - self.offset.get(), false));
         }
@@ -113,7 +107,7 @@ impl Arena {
     }
 
     pub fn ptr_for(&self, alloc: &ArenaAlloc) -> *mut u8 {
-        unsafe { self.base_ptr.as_ptr().add(alloc.offset as usize) }
+        unsafe { self.buf.as_ptr().add(alloc.offset as usize) }
     }
 
     pub fn get_alloc_slice(&self, alloc: &ArenaAlloc) -> ArenaAllocSlice {
@@ -141,7 +135,7 @@ impl Arena {
 
             while self.offset.get() > 0 {
                 let top_addr = self
-                    .base_ptr
+                    .buf
                     .as_ptr()
                     .add((self.offset.get() - FOOTER_SIZE) as usize)
                     .cast::<AllocFooter>();
@@ -158,7 +152,7 @@ impl Arena {
 
 impl Drop for Arena {
     fn drop(&mut self) {
-        let layout = Layout::from_size_align(self.max_capacity as usize, 16).unwrap();
-        unsafe { dealloc(self.base_ptr.as_ptr(), layout) };
+        let layout = Layout::from_size_align(self.capacity as usize, 16).unwrap();
+        unsafe { dealloc(self.buf.as_ptr(), layout) };
     }
 }
